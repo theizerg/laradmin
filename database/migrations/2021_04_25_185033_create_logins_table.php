c34b82fccfee72061c0ba3b7aa1bad3d55415f96
danielvera987 <danielveraangulo703@gmail.com>
2021-05-14 11:05:22 -0500

    fix migration

--- database/migrations/2021_04_25_185033_create_logins_table.php
+++ database/migrations/2021_04_25_185033_create_logins_table.php
@@ -15,8 +15,8 @@ class CreateLoginsTable extends Migration
     {
         Schema::create('logins', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('user_id')->unsigned();
-            $table->foreign('user_id')->references('id')->on('users');
+            //$table->integer('user_id')->unsigned();
+            $table->foreignId('user_id')->references('id')->on('users');
             $table->string('user_agent');
             $table->string('session_token', 40);
             $table->string('ip_address', 40);
