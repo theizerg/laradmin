0ec29db46218b9636da52d3d51ad7055d557a7e3
Theizer <theizerg@gmail.com>
2021-05-16 14:17:04 -0430

    fix Migraciones

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->string('name');
             $table->string('lastname');
             $table->string('username');
